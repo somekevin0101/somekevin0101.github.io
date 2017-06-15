2fb81b3bd9fd5f4068eb8b9132b2ca0e037914e1
Kevin Somerville <ksomerville0101@gmail.com>
2017-06-14 21:50:45 -0400

    button function

---
+++ Javascript.js
@@ -0,0 +1,10 @@
+﻿window.onload = init;
+
+function init() {
+    var button = document.getElementById("button");
+    button.onclick = handleButtonClick;
+}
+function handleButtonClick()
+{
+    location.href = 'https://github.com/somekevin0101/RPGDuo';
+}
